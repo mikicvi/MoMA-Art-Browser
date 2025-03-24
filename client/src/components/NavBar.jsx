614b25a11065ae821ec81d0712b6aa261f31a046
mikicvi <88291034+mikicvi@users.noreply.github.com>
2025-03-24 13:24:03 +0000

    implement responsive navbar toggle functionality

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function NavBar() {
+	const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+	const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
 	return (
 		<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
 			<div className='container'>
@@ -10,16 +15,14 @@ export default function NavBar() {
 				<button
 					className='navbar-toggler'
 					type='button'
-					data-bs-toggle='collapse'
-					data-bs-target='#navbarNav'
-					aria-controls='navbarNav'
-					aria-expanded='false'
+					onClick={handleNavCollapse}
+					aria-expanded={!isNavCollapsed}
 					aria-label='Toggle navigation'
 				>
 					<span className='navbar-toggler-icon'></span>
 				</button>
 
-				<div className='collapse navbar-collapse' id='navbarNav'>
+				<div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id='navbarNav'>
 					<ul className='navbar-nav ms-auto'>
 						<li className='nav-item'>
 							<Link to='/' className='nav-link'>
